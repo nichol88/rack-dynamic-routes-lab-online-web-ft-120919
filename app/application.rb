47f8d33e2b0a21cd4298a841b1f0f6ed6f3fae24
Brian Nicholls <nichol88@gmail.com>
2020-01-26 08:19:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,11 @@
 class Application
+  @@items = []
   def call(env)
     response = Rack::Response.new
     request = Rack::Request.new(env)
 
     if response.path.match(/items\/\w*/)
-
+      respponse.write
     end
     response.finish
   end
